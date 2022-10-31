aef79c7cbe93721d41b419f24102539fadc924b4
patdz <patdz@ymipro.com>
2022-10-31 22:27:41 +0800

    按需引入ant
    npm i --save ant-design-vue@next
    npm install babel-plugin-import --save-dev
    
    > http://www.javashuo.com/article/p-fzpluwvz-nx.html
    > https://www.antdv.com/docs/vue/introduce-cn 注意左上角选版本

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <a-button>我是</a-button>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -33,6 +34,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
